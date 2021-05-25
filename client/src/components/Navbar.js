c47634a1fc88306ca85f2b4d432fd1ff931a0392
Pankaj Maurya <pankaj@techalchemy.co>
2021-05-26 02:58:25 +0530

    admin server setup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded ">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="#">PizzaHut</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,12 +1,19 @@
 import React,{useState} from 'react'
+import {Modal} from 'react-bootstrap';
 
 export default function Pizza({pizzadata}) {
     const [quantity,setquantity] = useState(1);
     const [varient,setvarient] = useState('small');
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <div className="m-5 shadow-lg p-3 mb-5 bg-white rounded">
-            <h1>{pizzadata.name}</h1>
-            <img src={pizzadata.image} className="img-fluid "  style={{height:'200px',width:'200px'}}/>
+            <div onClick={handleShow}>
+                <h1>{pizzadata.name}</h1>
+                <img src={pizzadata.image} className="img-fluid "  style={{height:'200px',width:'200px'}}/>
+            </div>
             <div className="flex-container">
                 <div className="w-100 m-1">
                     <p>Varients</p>
@@ -34,6 +41,22 @@ export default function Pizza({pizzadata}) {
                     <button className="btn btn-danger">Add To Cart</button>
                 </div>
             </div>
+
+            <Modal show={show} onHide={handleClose}>
+            <Modal.Header>
+            <Modal.Title>{pizzadata.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <div className="model_big_image">
+            <img src={pizzadata.image} className="img-fluid "  style={{height:'300px',width:'300px'}}/>
+            </div>
+            <p>{pizzadata.description}</p>
+            </Modal.Body>
+            <Modal.Footer>
+                <button onClick={handleClose} className="btn btn-danger">Close</button>
+            </Modal.Footer>
+        </Modal>
+
         </div>
     )
 }
